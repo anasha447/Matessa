c5f0cd12b9ea6e6912f8c5f2993a8203ea9f189d
anasha447 <anashabib0101@gmail.com>
2026-01-13 01:16:05 +0530

    google analytics setup

--- matessa/src/main/java/com/ecommerce/matessa/security/SecurityConfig.java
+++ matessa/src/main/java/com/ecommerce/matessa/security/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -61,6 +62,22 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(
+                "/assets/**",
+                "/static/**",
+                "/images/**",
+                "/favicon.ico",
+                "/*.js",
+                "/*.css",
+                "/*.png",
+                "/*.jpg",
+                "/manifest.json",
+                "/index.html"
+        );
     }
 
     @Bean
